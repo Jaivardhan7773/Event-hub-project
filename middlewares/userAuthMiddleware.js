357101b1c15e565fc0ba9d7bb2fcfe10ccb9c0cb
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-07-01 23:13:10 +0530

    fixed check route's middleware

--- middlewares/userAuthMiddleware.js
+++ middlewares/userAuthMiddleware.js
@@ -5,7 +5,9 @@ import User from "../models/userModel.js";
 //middleare for updating profile for now
 export const userAuthMiddleware = async (req, res, next) => {
     try {
-        const token = req.cookies.jwt
+        // console.log("Cookies: ", req.cookies);
+        // console.log("Token: ", req.cookies.jwt);
+        const token = req.cookies.token
         if (!token) {
             return res.status(401).json({
                 message: "Cannot get token"
@@ -18,7 +20,9 @@ export const userAuthMiddleware = async (req, res, next) => {
                 message: "Invalid token"
             })
         }
-        const user = await User.findById(decoded.userId).select("-password");
+        // console.log("Decoded token:", decoded);
+
+        const user = await User.findById(decoded.id).select("-password");
         if (!user) {
             return res.status(404).json({
                 message: "Invalid user"
