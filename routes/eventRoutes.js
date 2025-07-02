a5c31a0ed4482b769b7349819cc19c546ecbf1f7
Jaivardhan7773 <jaivardhansinghrathore17@gmail.com>
2025-07-02 21:12:59 +0530

    added creating event model and route

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,6 +4,9 @@ import { userAuthMiddleware } from '../middlewares/userAuthMiddleware.js';
 
 
 const router = express.Router();
+//create an event
+router.post("/", userAuthMiddleware, addEvent)
+
 
 // Register for event (student)
 router.post('/:id/register', userAuthMiddleware, registerForEvent);
